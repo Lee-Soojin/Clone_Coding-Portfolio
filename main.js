db041c74dd88d0344507402c7cbf790993d7eb89
Soojin <soojl1005@naver.com>
2021-03-16 21:48:09 +0900

    JS: Navbar click event + contact button click event (myself )

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 "use strict";
 
 // Make navbar transparent when it is on the top
-const navbar = document.getElementById("navbar");
-const navbarHeight = navbar.getBoundingClientRect().height;
+const navbar = document.getElementById("navbar"),
+  navbarHeight = navbar.getBoundingClientRect().height,
+  BtnContact = document.querySelector(".home__contact"),
+  sect_Contact = document.getElementById("#contact");
 
 document.addEventListener("scroll", () => {
   if (window.scrollY >= navbarHeight) {
@@ -10,4 +12,20 @@ document.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("navbar--dark");
   }
+});
+
+// when button clicked, scroll to each section
+
+navbar.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  const elem = document.querySelector(link);
+  elem.scrollIntoView({ behavior: "smooth" });
+});
+
+BtnContact.addEventListener("click", (event) => {
+  const btntarget = event.target;
+  const contact_link = btntarget.dataset.link;
+  const contact_elem = document.querySelector(contact_link);
+  contact_elem.scrollIntoView({ behavior: "smooth" });
 });
